abf87387f2e0f80164305e53bfa8dc4161cb5191
Kyle Jensen <kjensen@alertlogic.com>
2016-04-26 11:08:27 -0500

    Add popular movies to home.

--- src/main/tmdb/partials/home/HomeController.js
+++ src/main/tmdb/partials/home/HomeController.js
@@ -13,15 +13,18 @@
  * @copyright Alert Logic, 2016
  */
 
-define( [ 'angular' ],
-    function( angular ) {
+define( [ 'angular', 'config/config', 'tmdb/services/TMDBAPIService' ],
+    function( angular, config, TMDBAPIService ) {
         "use strict";
 
-        var ctrl = function($scope) {
-            $scope.data = "Something scope-y.";
+        var ctrl = function($scope, TMDBAPIService) {
+            $scope.imgPath = config.apiImg;
+            TMDBAPIService.Discover().discover.movies().then(function (res) {
+                $scope.popMovieList = res.data.results;
+            });
         };
 
-        ctrl.$inject = ['$scope'];
+        ctrl.$inject = ['$scope', 'TMDBAPIService'];
 
         return ctrl;
     }
